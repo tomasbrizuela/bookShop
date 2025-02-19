a024f5d9b392cef8d8bec64a427dade6b5c809d2
Tomi <tomasagustinbrizuela@gmial.com>
2025-02-19 09:24:50 -0300

    persistencia

---
+++ routes/cart.routes.js
@@ -0,0 +1,39 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+    try {
+        let cart = cartList;
+        res.status(200).send({ "Cart": cart })
+    } catch (error) {
+
+    }
+})
+
+router.post("/", (req, res) => {
+    let product = req.body;
+    try {
+        cartList.push(product);
+        res.status(201).json({ "Status": "Producto agregado al carrito" })
+    } catch (error) {
+        res.status(400).json({ "Status": "Hubo un error" })
+    }
+})
+
+export default router;
+
+const cartList = [
+    {
+        "id": 1,
+        "cart": [1, 3, 5]
+    },
+    {
+        "id": 2,
+        "cart": [2, 4, 6, 10]
+    },
+    {
+        "id": 3,
+        "cart": [1, 2, 3, 7, 8]
+    }
+]
