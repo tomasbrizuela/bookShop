824bc020c12de95092dc6e5a674b1cdcd5f55d4a
Tomi <tomasagustinbrizuela@gmial.com>
2025-02-26 09:21:01 -0300

    agregué el endpoint html con una plantilla

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import morgan from 'morgan';
+import handlebars from 'express-handlebars';
+import { __dirname } from '../Utils/pathDir.js';
+import viewsRouter from '../routes/views.router.js'
+import cartsRouter from '../routes/cart.routes.js'
+import productsRouter from '../routes/product.routes.js'
 
 const PORT = 8080
 const app = express()
+
 app.use(morgan('dev'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-
 
 app.get('/health', (req, res) => {
     try {
@@ -16,13 +21,16 @@ app.get('/health', (req, res) => {
     }
 })
 
-import cartsRouter from '../routes/cart.routes.js'
-import productsRouter from '../routes/product.routes.js'
 app.use('/api/cart', cartsRouter);
 app.use('/api/product', productsRouter);
+app.use('/', viewsRouter);
 
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars')
 
 app.listen(PORT, () => {
-    console.log("Hola")
     console.log(`http://localhost:${PORT}`)
 })
+
+//Voy por el minuto 1:26:00
